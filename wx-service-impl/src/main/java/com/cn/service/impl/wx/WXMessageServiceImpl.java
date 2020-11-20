c18ae78ea3cca32b0b9020056dba3b452fa4dde3
shazhibin <shazhibin07@163.com>
2020-11-20 17:05:11 +0800

    添加消息接收

--- wx-service-impl/src/main/java/com/cn/service/impl/wx/WXMessageServiceImpl.java
+++ wx-service-impl/src/main/java/com/cn/service/impl/wx/WXMessageServiceImpl.java
@@ -3,6 +3,9 @@ package com.cn.service.impl.wx;
 import com.cn.beans.wx.WXConstant;
 import com.cn.service.wx.WXMessageService;
 import org.apache.commons.lang3.StringUtils;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -17,11 +20,24 @@ public class WXMessageServiceImpl implements WXMessageService {
             String content = messageMap.get("Content");
             if (!StringUtils.isBlank(content)) {
                 if (content.contains("约课")) {
-                    return "<a src='http://www.shazhibin.top/service/appointment.html'>约课</a>";
+                    messageMap.put("Content", "<a href='http://www.shazhibin.top/service/appointment.html'>约课</a>");
+                    return messageParseXml(messageMap);
                 }
             }
         }
-        return "消息错误";
+        messageMap.put("Content", "消息错误");
+        return messageParseXml(messageMap);
+
+    }
+
+    private static String messageParseXml(Map<String, String> map) {
+        Document document = DocumentHelper.createDocument();
+        Element root = document.addElement("xml");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            Element element = root.addElement(entry.getKey());
+            element.add(DocumentHelper.createCDATA(entry.getValue()));
+        }
+        return document.toString();
 
     }
 }
